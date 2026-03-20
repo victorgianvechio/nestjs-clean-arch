17dbf93cb3cb91e3dfd269e017b1b0f4336f05d3
Victor Gianvechio <victor.gianvecchio@live.com>
2026-03-19 21:16:12 -0300

    UserEntity - getter methods unit test

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -7,6 +7,23 @@ export type UserProps = {
 
 export class UserEntity {
   constructor(public readonly props: UserProps) {
+    this.props = props;
     this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
   }
 }
